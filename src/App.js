a19a68df44b1f761350fea3366a54e48f9cf9f0a
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-16 01:05:12 +0530

    prettier file fix

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Woooho! Netflix Clone!
-      </h1>
+      <h1>Woooho! Netflix Clone!</h1>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBfdDMdRf_YA75eYALQnMijYFcprmx6XS0",
-    authDomain: "netflix-clone-500d3.firebaseapp.com",
-    projectId: "netflix-clone-500d3",
-    storageBucket: "netflix-clone-500d3.appspot.com",
-    messagingSenderId: "753374719639",
-    appId: "1:753374719639:web:5580e04e58f646992003d2",
-    measurementId: "G-E6ZCNCB4BH"
-  };
+  apiKey: "AIzaSyBfdDMdRf_YA75eYALQnMijYFcprmx6XS0",
+  authDomain: "netflix-clone-500d3.firebaseapp.com",
+  projectId: "netflix-clone-500d3",
+  storageBucket: "netflix-clone-500d3.appspot.com",
+  messagingSenderId: "753374719639",
+  appId: "1:753374719639:web:5580e04e58f646992003d2",
+  measurementId: "G-E6ZCNCB4BH",
+};

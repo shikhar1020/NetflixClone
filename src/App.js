2c4840b0ed6ec38f0a2994a6bb7c1091734aa239
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-19 18:55:50 +0530

    initialised homepage route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
 import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
-      <HomeScreen />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <HomeScreen />
+          </Route>
+          {/* <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route> */}
+        </Switch>
+      </Router>
     </div>
   );
 }

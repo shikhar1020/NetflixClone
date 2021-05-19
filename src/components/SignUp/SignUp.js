4a6868832251a4795750229adc77190022418de8
Shikhar Srivastava <54438024+shikhar1020@users.noreply.github.com>
2021-05-20 00:06:19 +0530

    sign up and sign In with email

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,17 +1,61 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { auth } from "../../firebase";
 import "./style.css";
 
 function SignUp() {
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  useEffect(() => {
+    console.log(auth);
+  }, []);
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+        alert(`Sign In Sucessufull ${emailRef.current.value}`);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
+  };
+
+  const signIn = (e) => {
+    e.preventDefault();
+
+    auth
+      .signInWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+        alert(`Sign In Sucessufull ${emailRef.current.value}`);
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="signUpScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Sign In</button>
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
+        <button onClick={signIn} type="submit">
+          Sign In
+        </button>
         <h4>
           <span className="newToNetflix">New to Netflix? </span>
-          <span className="signUpLink"> Sign up now</span>
+          <span onClick={register} className="signUpLink">
+            Sign up now
+          </span>
         </h4>
       </form>
     </div>
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,5 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBfdDMdRf_YA75eYALQnMijYFcprmx6XS0",
   authDomain: "netflix-clone-500d3.firebaseapp.com",
@@ -8,3 +9,10 @@ const firebaseConfig = {
   appId: "1:753374719639:web:5580e04e58f646992003d2",
   measurementId: "G-E6ZCNCB4BH",
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+export { auth };
+export default db;
